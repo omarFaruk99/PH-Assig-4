542e055c5c65c76f163c8449b63b5b65dd206f5f
faruk <2035faruk@gmail.com>
2024-09-11 22:40:30 +0600

    complete problem-5: 'waitingTime'

---
+++ problem-5.js
@@ -0,0 +1,22 @@
+function waitingTime(waitingTimes, serialNumber) {
+    if (!Array.isArray(waitingTimes) || typeof serialNumber !== "number") {
+        return "Invalid Input"
+    } else {
+        let watingTimeSum = 0;
+        for (const i of waitingTimes) {
+            watingTimeSum = watingTimeSum + i;
+        }
+        const avgWaitingTime = Math.round(watingTimeSum / waitingTimes.length);
+        const alreayDone = waitingTimes.length;
+        const beforePersonHave = (serialNumber - 1) - alreayDone;
+        const personWaitingTimeNeed = avgWaitingTime * beforePersonHave;
+        return personWaitingTimeNeed;
+    }
+
+}
+
+
+// let arr = 7;
+// let serialNum = 10;
+// let fun = waitingTime(arr, serialNum);
+// console.log(fun);
